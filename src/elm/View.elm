de5870075e8c52713498cbfc9612a7e73cba0057
Esad Hajdarevic <esad@esse.at>
2015-12-07 23:23:15 +0100

    Add col and row headers and set/style selected state for each

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -1,14 +1,19 @@
 module View (view) where 
 
-import Html exposing (Html, span, text, table, tr, td, button, input)
+import Html exposing (Html, span, text, table, thead, tbody, th, tr, td, button, input)
 import Html.Attributes exposing (classList, value, key)
 import Html.Events exposing (onClick, onDoubleClick)
 import Signal
+import Maybe
 
 import Helpers
 import Model exposing (Action(..))
 import Sheet
 import Addr
+
+type Header = RowHeader | ColHeader
+
+nbsp = "\xa0" 
 
 view : Signal.Address Action -> Model.Model -> Html
 view address model =
@@ -28,7 +33,7 @@ view address model =
               ]
               [
                 Sheet.cell2str cell
-                  |> Maybe.withDefault "\xa0" -- &nbsp;
+                  |> Maybe.withDefault nbsp
                   |> text
               ]
           Just editStr ->
@@ -39,12 +44,39 @@ view address model =
               [
                 Helpers.cellInput editStr address (\str -> Commit addr str) Cancel
               ]
+    viewHeader header idx =
+      let
+        (component, identifier, tag) = case header of
+          RowHeader -> (Addr.row, Addr.rowIdentifier, td)
+          ColHeader -> (Addr.col, Addr.colIdentifier, th)
+        selected = (component model.selection == idx)
+      in
+        tag
+          [ classList [("selected", selected)]]
+          [ 
+            identifier idx
+            |> text
+          ]
     viewRow row cols =
       cols
         |> List.indexedMap (\col cell -> viewCell (Addr.fromColRow col row) cell)
+        |> (::) (viewHeader RowHeader row)
         |> tr []
-  in
-    model.sheet
-      |> Sheet.toList
+    sheet =
+      Sheet.toList model.sheet
+    colHeader = 
+      sheet
+      |> List.head 
+      |> Maybe.withDefault []
+      |> List.indexedMap (\col _ -> viewHeader ColHeader col)
+      |> (::) (th [] [text nbsp]) -- corner
+      |> tr []
+    body =
+      sheet
       |> List.indexedMap viewRow
-      |> table []
+      |> tbody []
+  in
+    table [] 
+      [ colHeader
+      , body
+      ]
