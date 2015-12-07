3f6ad77a8eca3da671f2db0e50767918e516d231
Esad Hajdarevic <esad@esse.at>
2015-12-07 01:28:20 +0100

    Implement Addr.identifier, preparing for cell names

--- src/elm/Addr.elm
+++ src/elm/Addr.elm
@@ -1,4 +1,8 @@
-module Addr (Addr, row, col, fromColRow, add) where
+module Addr (Addr, row, col, fromColRow, add, identifier, rowIdentifier, colIdentifier) where
+
+import Array
+import Char
+import String
 
 type Addr = Addr Int Int
 
@@ -16,4 +20,38 @@ fromColRow col row =
 
 add : Addr -> Addr -> Addr
 add a b =
-  fromColRow (col a + col b) (row a + row b) 
+  fromColRow (col a + col b) (row a + row b)
+
+alphabet : Array.Array Char
+alphabet = 
+  Array.initialize 26 (((+) 65) >> Char.fromCode)
+
+toBase : Int -> Int -> List Int
+toBase b v = 
+  let toBase' list v =
+    if v == 0 then
+      list
+    else
+      toBase' (v `rem` b :: list) (v // b)
+  in
+    if v == 0 then
+      [0]
+    else
+      toBase' [] v
+
+colIdentifier : Int -> String
+colIdentifier col =
+  col
+  |> toBase (Array.length alphabet)
+  |> List.filterMap (\n -> Array.get n alphabet)
+  |> String.fromList
+
+rowIdentifier : Int -> String
+rowIdentifier row =
+  row + 1 |> toString 
+
+identifier : Addr -> String
+identifier addr =
+  (col addr |> colIdentifier)
+  ++
+  (row addr |> rowIdentifier)
